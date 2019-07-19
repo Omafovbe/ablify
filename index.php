1279994cf3cdf71a2a3460d42a437ae36aeaabf8
abilfy <flesktechsaas@gmail.com>
2019-07-19 19:23:51 +0100

    Ablify make it better

---
+++ index.php
@@ -0,0 +1,491 @@
+<?php 
+include 'header.php';
+?>
+
+<!-- banner -->
+<div class="banner" id="home">
+	<div class="layer">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-6 banner-text-w3ls">
+					<!-- banner slider-->
+					<div class="csslider infinity" id="slider1">
+						<input type="radio" name="slides" checked="checked" id="slides_1" />
+						<input type="radio" name="slides" id="slides_2" />
+						<input type="radio" name="slides" id="slides_3" />
+						<ul class="banner_slide_bg">
+							<li>
+								<div class="container-fluid">
+									<div class="w3ls_banner_txt">
+										<h3 class="b-w3ltxt text-capitalize mt-md-4">china</h3>
+										<h4><span class="fa fa-ils" aria-hidden="true"></span>from</h4>
+									<p><span class="fa fa-usd" aria-hidden="true"></span>1000<p>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="container-fluid">
+									<div class="w3ls_banner_txt">
+										<h3 class="b-w3ltxt text-capitalize mt-md-4">japan</h3>
+										<h4><span class="fa fa-ils" aria-hidden="true"></span>from </h4>
+									<p><span class="fa fa-usd" aria-hidden="true"></span>1200<p>
+										
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="container-fluid">
+									<div class="w3ls_banner_txt">
+										<h3 class="b-w3ltxt text-capitalize mt-md-4">korea</h3>
+										<h4><span class="fa fa-ils" aria-hidden="true"></span>from </h4>
+										<p><span class="fa fa-usd" aria-hidden="true"></span>1400<p>
+									</div>
+								</div>
+							</li>
+						</ul>
+						<div class="navigation">
+							<div>
+								<label for="slides_1"></label>
+								<label for="slides_2"></label>
+								<label for="slides_3"></label>
+							</div>
+						</div>
+					</div>
+					<!-- //banner slider-->
+				</div>
+				<div class="col-md-6 px-lg-3 px-0">
+					<div class="banner-form-w3 ml-lg-5">
+						<div class="padding">
+							<form action="#" method="post">
+								<h5 class="mb-3">Search Our Tours</h5>
+								<div class="form-style-ablify">
+								<input placeholder="Search Location" name="name" type="text" required="">
+					            <input placeholder="Price Range" name="name" type="text" required="">
+									<button Class="btn"> Search</button>
+									
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- //banner -->
+  <!-- banner-bottom -->
+    <section class="some-content py-5" id="about">
+        <div class="container py-md-5">
+            <div class="row about-vv-top mt-2">
+                <div class="col-lg-6 about-info">
+                    <h4 class="title-hny  mb-md-5">Winter Wonders and Delicious Detours</h4>
+                    <p>Donec malesuada ex sit amet pretium sid ornare. Nulla congue scelerisque tellus, utpretium nulla malesuada sedint. Suspendisse venenatis,Lorem ipsum dolor sit magna dolor.</p>
+                    <div class="read-more-button mt-4">
+                        <a href="index.html" class="read-more btn">Read More </a>
+                    </div>
+
+                </div>
+                <div class="col-lg-6 about-img mt-md-4 mt-sm-4">
+                    <img src="images/ab1.jpg" class="img-fluid" alt="">
+                </div>
+
+            </div>
+        </div>
+    </section>
+    <!-- //banner-bottom-->
+	<!-- services -->
+<section class="services py-5" id="services">
+	<div class="container py-md-5">
+	<h3 class="heading text-center mb-3 mb-sm-5">Our services</h3>
+		<div class="row service-grid-grids text-center">
+			<div class="col-lg-4 col-md-6 service-grid service-grid1 mb-4">
+				<div class="service-icon">
+					<span class="fa fa-h-square"></span>
+				</div>
+				<h4 class="mt-3">Best hotels</h4>
+				<p class="mt-3">Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.</p>
+			</div>
+			<div class="col-lg-4 col-md-6 service-grid service-grid2 mb-4">
+				<div class="service-icon">
+					<span class="fa fa-glide-g"></span>
+				</div>
+				<h4 class="mt-3">Tourist guide</h4>
+				<p class="mt-3">Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.</p>
+			</div>
+			
+			<div class="col-lg-4 col-md-6 service-grid service-grid3 mb-4">
+				<div class="service-icon">
+					<span class="fa fa-fighter-jet"></span>
+				</div>
+				<h4 class="mt-3">Flight tickets</h4>
+				<p class="mt-3">Perspiciatis unde omnis iste natus doloret ipsum volupte ut accusal ntium dolor remque laudantium, totam dolor.</p>
+			</div>
+		</div>
+		<div class="row mt-5">
+			<div class="col-md-6 p-md-0 mb-4">
+				<div class="bg-image-left">	
+					<h4>Canada Tours</h4>
+				</div>
+			</div>
+			<div class="col-md-6 p-md-0 mb-4">
+				<div class="bg-image-right">
+					<h4>Asia Tours</h4>
+				</div>
+				<div class="row">
+					<div class="col-md-6 pr-md-0">
+						<div class="bg-image-bottom1">
+							<h4>Europe Tours</h4>
+						</div>
+					</div>
+					<div class="col-md-6 pl-md-0">
+						<div class="bg-image-bottom2">
+							<h4>Canada Tours</h4>
+						</div>
+					</div>
+				</div>	
+			</div>	
+		</div>		
+	</div>		
+</section>
+<!-- //services -->
+ <!-- stats -->
+    <section class="stats" id="stats">
+	<div class="layer py-md-5 py-5">
+        <div class="container py-lg-5 py-md-3">
+            <div class="row stat-grids">
+                <div class="col-lg-6 stats-left">
+                    <h3 class="heading mb-4 text-li">Years in the field</h3>
+                    <p class="mb-3">Integer pulvinar leo id viverra feugiat. Pellentesque libero justo, semper at tempus vel, ultrices in ligula. Nulla sollicit sed.</p>
+                    <h4><span>15+</span> Years of Experience</h4>
+                </div>
+                <div class="col-lg-6 grid1 stats-right mt-lg-0 mt-4 pl-5">
+                    <div class="row">
+                        <div class="col-sm-4 col-6 mb-4">
+                            <p class="text-li">Travel joy</p>
+                            <h4 class="text-wh">12k</h4>
+                            <span class="fa fa-users mr-2"></span>
+                        </div>
+                        <div class="col-sm-4 col-6 mb-4">
+                            <p>world tours</p>
+                            <h4>15k</h4>
+                            <span class="fa fa-tasks mr-2"></span>
+                        </div>
+                        <div class="col-sm-4 col-6 mb-4">
+                            <p>city tours</p>
+                            <h4>25k</h4>
+                            <span class="fa fa-files-o mr-2"></span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+		</div>
+    </section>
+    <!-- //stats -->
+<!-- Team  -->
+<section class="team py-5" id="team">
+    <div class="container py-md-5">
+		<h3 class="heading text-center mb-3 mb-sm-5">Our Guides</h3>
+        <div class="row">
+			<div class="team-grid col-lg-3 col-sm-6 mb-5">
+				<img src="images/t1.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="e">Tyson Amery</h3>
+					<span class="">Manager</span>
+				</div>
+			</div>
+			<div class="team-grid col-lg-3 col-sm-6  mb-5">
+				<img src="images/t2.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="">Stas Melnik</h3>
+					<span class="">director</span>
+				</div>
+			</div>
+			<div class="team-grid col-lg-3 col-sm-6  mb-5">
+				<img src="images/t3.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="">Lise Laurie</h3>
+					<span class="">Instructor</span>
+				</div>
+			</div>
+			<div class="team-grid col-lg-3 col-sm-6  mb-5">
+				<img src="images/t4.jpg" class="" alt="" />
+				<div class="team-info text-center">
+					<h3 class="">Effie Eleanora</h3>
+					<span class="">manager</span>
+				</div>
+			</div>
+        </div>
+    </div>
+</section>
+<!-- //Team -->
+
+ <!-- Gallery -->
+    <section class="gallery py-5" id="gallery">
+        <div class="container py-md-5">
+            <div class="header mb-3 mb-sm-5 text-center">
+               <h3 class="heading text-center mb-3 mb-sm-5">Tour Gallery</h3>
+                <div class="row news-grids text-center">
+                    <div class="col-md-4 gal-img">
+                        <a href="#gal1"><img src="images/g1.jpg" alt="news image" class="img-fluid"><span> HoneyMoon</span></a>
+                        <a href="#gal2"><img src="images/g2.jpg" alt="news image" class="img-fluid"><span>Family</span></a>
+
+                    </div>
+
+                    <div class="col-md-4 gal-img">
+                        <a href="#gal3"><img src="images/g3.jpg" alt="news image" class="img-fluid"><span>Holidays</span></a>
+
+                        <a href="#gal4"><img src="images/g4.jpg" alt="news image" class="img-fluid"><span>Adventure</span></a>
+                    </div>
+                    <div class="col-md-4 gal-img">
+                        <a href="#gal5"><img src="images/g5.jpg" alt="news image" class="img-fluid"><span>Luxury</span></a>
+                        <a href="#gal6"><img src="images/g6.jpg" alt="news image" class="img-fluid"><span>Romantic</span></a>
+                    </div>
+                </div>
+            </div>
+            <!-- popup-->
+            <div id="gal1" class="pop-overlay animate">
+                <div class="popup">
+                    <img src="images/g1.jpg" alt="Popup Image" class="img-fluid" />
+                    <p class="mt-4">Nulla viverra pharetra se, eget pulvinar neque pharetra ac int. placerat placerat dolor.</p>
+                    <a class="close" href="#gallery">&times;</a>
+                </div>
+            </div>
+            <!-- //popup -->
+
+            <!-- popup-->
+            <div id="gal2" class="pop-overlay animate">
+                <div class="popup">
+                    <img src="images/g2.jpg" alt="Popup Image" class="img-fluid" />
+                    <p class="mt-4">Nulla viverra pharetra se, eget pulvinar neque pharetra ac int. placerat placerat dolor.</p>
+                    <a class="close" href="#gallery">&times;</a>
+                </div>
+            </div>
+            <!-- //popup -->
+            <!-- popup-->
+            <div id="gal3" class="pop-overlay animate">
+                <div class="popup">
+                    <img src="images/g3.jpg" alt="Popup Image" class="img-fluid" />
+                    <p class="mt-4">Nulla viverra pharetra se, eget pulvinar neque pharetra ac int. placerat placerat dolor.</p>
+                    <a class="close" href="#gallery">&times;</a>
+                </div>
+            </div>
+            <!-- //popup3 -->
+            <!-- popup-->
+            <div id="gal4" class="pop-overlay animate">
+                <div class="popup">
+                    <img src="images/g4.jpg" alt="Popup Image" class="img-fluid" />
+                    <p class="mt-4">Nulla viverra pharetra se, eget pulvinar neque pharetra ac int. placerat placerat dolor.</p>
+                    <a class="close" href="#gallery">&times;</a>
+                </div>
+            </div>
+            <!-- //popup -->
+            <!-- popup-->
+            <div id="gal5" class="pop-overlay animate">
+                <div class="popup">
+                    <img src="images/g5.jpg" alt="Popup Image" class="img-fluid" />
+                    <p class="mt-4">Nulla viverra pharetra se, eget pulvinar neque pharetra ac int. placerat placerat dolor.</p>
+                    <a class="close" href="#gallery">&times;</a>
+                </div>
+            </div>
+            <!-- //popup -->
+            <!-- popup-->
+            <div id="gal6" class="pop-overlay animate">
+                <div class="popup">
+                    <img src="images/g6.jpg" alt="Popup Image" class="img-fluid" />
+                    <p class="mt-4">Nulla viverra pharetra se, eget pulvinar neque pharetra ac int. placerat placerat dolor.</p>
+                    <a class="close" href="#gallery">&times;</a>
+                </div>
+            </div>
+            <!-- //popup -->
+        </div>
+    </section>
+    <!--// gallery -->
+	
+<!-- destinations -->
+<section class="destinations py-5" id="destinations">
+	<div class="container py-md-5">
+		  <h3 class="heading text-center mb-3 mb-sm-5">Tour Prices</h3>
+		<div class="row inner-sec-ablify-w3pvt-lauinfo">
+			<div class="col-md-3 col-sm-6 col-6 destinations-grids text-center mb-4">
+				<h4 class="destination mb-3">China</h4>
+				<div class="image-position position-relative">
+					<img src="images/p1.jpg" class="img-fluid" alt="">
+					<div class="rating">
+						<ul>
+							<li><span class="fa fa-usd"></span>1000</li>
+							
+						</ul>
+					</div>
+				</div>
+				<div class="destinations-info">
+					<div class="caption mb-lg-3">
+						<h4>China</h4>
+						<a href="#contact">Book Now</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 destinations-grids text-center mb-4">
+				<h4 class="destination mb-3">Malaysia</h4>
+				<div class="image-position position-relative">
+					<img src="images/p2.jpg" class="img-fluid" alt="">
+					<div class="rating">
+						<ul>
+							<li><span class="fa fa-usd"></span>1100</li>
+						</ul>
+					</div>
+				</div>
+				<div class="destinations-info">
+					<div class="caption mb-lg-3">
+						<h4>Malaysia</h4>
+						<a href="#contact">Book Now</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 destinations-grids text-center mb-4">
+				<h4 class="destination mb-3">Japan</h4>
+				<div class="image-position position-relative">
+					<img src="images/p3.jpg" class="img-fluid" alt="">
+					<div class="rating">
+						<ul>
+							<li><span class="fa fa-usd"></span>1200</li>
+						</ul>
+					</div>
+				</div>
+				<div class="destinations-info">
+					<div class="caption mb-lg-3">
+						<h4>Japan</h4>
+						<a href="#contact">Book Now</a>
+					</div>
+				</div>
+			</div>
+			<div class="col-md-3 col-sm-6 col-6 destinations-grids text-center mb-4">
+				<h4 class="destination mb-3">Singapore</h4>
+				<div class="image-position position-relative">
+					<img src="images/p4.jpg" class="img-fluid" alt="">
+					<div class="rating">
+						<ul>
+							<li><span class="fa fa-usd"></span>1300</li>
+						</ul>
+					</div>
+				</div>
+				<div class="destinations-info">
+					<div class="caption mb-lg-3">
+						<h4>Singapore</h4>
+						<a href="#contact">Book Now</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- destinations -->
+ <!--/testimonials -->
+    <section class="testimonials py-5" id="testimonials">
+        <div class="container py-md-5">
+                <h3 class="heading heading1 text-center mb-3 mb-sm-5"> Client Reviews</h3>
+            <div class="row">
+                <div class="col-lg-4 col-sm-6 test-info text-left mb-4">
+                    <p><span class="fa fa-quote-left"></span> Lorem Ipsum has been the industry's standard since the 1500s. Praesent ullamcorper dui turpis.Nulla non laoreet eleifend. <span class="fa fa-quote-right"></span></p>
+                    <div class="test-img text-right mb-3">
+                        <img src="images/te1.jpg" class="img-fluid" alt="user-image">
+                    </div>
+                    <h3 class="my-md-2 my-3 text-right">Jenifer Burns</h3>
+
+                   
+                </div>
+                <div class="col-lg-4 col-sm-6 test-info text-left mb-4">
+                    <p><span class="fa fa-quote-left"></span> Lorem Ipsum has been the industry's standard since the 1500s. Praesent ullamcorper dui turpis.Nulla non laoreet eleifend. <span class="fa fa-quote-right"></span></p>
+
+                    <div class="test-img text-right mb-3">
+                        <img src="images/te2.jpg" class="img-fluid" alt="user-image">
+                    </div>
+                    <h3 class="my-md-2 my-3 text-right"> Abraham Smith</h3>
+
+                   
+                </div>
+                <div class="col-lg-4 col-sm-6 test-info text-left gap1 mb-4">
+                    <p><span class="fa fa-quote-left"></span> Lorem Ipsum has been the industry's standard since the 1500s. Praesent ullamcorper dui turpis.Nulla non laoreet eleifend. <span class="fa fa-quote-right"></span></p>
+                    <div class="test-img text-right mb-3">
+                        <img src="images/te3.jpg" class="img-fluid" alt="user-image">
+                    </div>
+                    <h3 class="my-md-2 my-3 text-right">Jenifer Burns</h3>
+
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--//testimonials -->
+<!-- Contact -->
+<section class="contact py-5" id="contact">
+	<div class="container py-md-5">
+			  <h3 class="heading text-center mb-3 mb-sm-5"> Get In Touch with us</h3>
+			<ul class="list-unstyled row text-center mt-lg-5 mt-4 px-lg-5">
+                <li class="col-md-4 col-sm-6 adress-w3pvt-info mb-4">
+                    <div class=" adress-icon">
+                        <span class="fa fa-map-marker"></span>
+                    </div>
+
+                    <h6>Location</h6>
+                    <p>The company name
+                        <br>Addison Township, IL, USA. </p>
+                </li>
+
+                <li class="col-md-4 col-sm-6 adress-w3pvt-info mb-4">
+                    <div class="adress-icon">
+                        <span class="fa fa-envelope-open-o"></span>
+                    </div>
+                    <h6>Phone & Email</h6>
+                    <p>+(122) 123 456 7890</p>
+                    <a href="mailto:info@example.com" class="mail">info@examplemail.com</a>
+                </li>
+				 <li class="col-md-4 col-sm-6 adress-w3pvt-info mb-4">
+                    <div class="adress-icon">
+                        <span class="fa fa-building"></span>
+                    </div>
+                    <h6>our branches</h6>
+                    <p>india</p>
+					 <p>china</p>
+                   
+                </li>
+            </ul>
+			
+			<div class="contact-grids mt-5">
+				<div class="row">
+					<div class="col-lg-6 col-md-6 contact-left-form">
+						<form action="#" method="post">
+							<div class=" form-group contact-forms">
+							  <input type="text" class="form-control" placeholder="Name" required="">
+							</div>
+							<div class="form-group contact-forms">
+							  <input type="email" class="form-control" placeholder="Email" required="">
+							</div>
+							<div class="form-group contact-forms">
+							  <input type="text" class="form-control" placeholder="Phone" required=""> 
+							</div>
+							<div class="form-group contact-forms">
+							  <textarea class="form-control" placeholder="Message" rows="3" required=""></textarea>
+							</div>
+							<button class="btn btn-block sent-butnn">Send</button>
+						</form>
+					</div>
+					<div class="col-lg-6 col-md-6 contact-right pl-lg-5">
+						 <img src="images/c1.jpg" class="img-fluid" alt="user-image">
+						
+					</div>
+				</div>
+			</div>
+	</div>
+</section>
+<!-- //Contact -->
+   <!-- map -->	
+<div class="map p-2">
+	<iframe src=""></iframe>
+</div>
+<!-- //map -->
+
+<!-- //contact us -->
+<?php
+include 'footer.php';
+?>
+ 
